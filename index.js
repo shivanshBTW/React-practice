96c5fcfafdbb3e8a0eb949197c10abe3a3ccbdc3
Shivansh Tyagi <stshivansh99@gmail.com>
2019-06-01 18:38:39 +0530

    CSS in react

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 class App extends React.Component {
   render() {
+    function getRandomInt(max) {
+      return Math.floor(Math.random() * Math.floor(max));
+    }
+    var s1 = getRandomInt(3);
+    var s2 = getRandomInt(3);
+    var s3 = getRandomInt(3);
+
     return (
       <div>
-        <Hello to="Raman" from="Sam" />
-        <Hello to="Alpha" exclamations={10} />
+        <h1>Slot Machine</h1>
+        <Machine m1={s1} m2={s2} m3={s3} />
       </div>
     );
   }
 }
 
-// function App() {
-
-// }
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.querySelector('#root'));
