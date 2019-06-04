130ed663d7ff7661f7b9964a8ad76145da6ba3a2
Shivansh Tyagi <stshivansh99@gmail.com>
2019-06-04 22:39:56 +0530

    setState and state example

---
+++ src/Game.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+export default class Game extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      score: 99,
+      gameOver: false
+    };
+    this.setState({ score: 0 });
+  }
+  render() {
+    return (
+      <div>
+        <h1> Your score is : {this.state.score}</h1>
+      </div>
+    );
+  }
+}
